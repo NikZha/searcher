decde90b201e9a0c075835c7e8da1f661043296f
NikWork <3rim.rzn@gmail.com>
2024-09-10 16:59:09 +0300

    add function js to autoemail on click

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ create table
     if not exists emailsubject (
         id bigint auto_increment,
         date_query date,
-        search_query varchar(100) not null,
+        search_query varchar(500) not null,
         finded_email varchar(1500) not null,
         finded_link varchar(1500) not null
     );
---
+++ src/main/resources/static/autoemail.js
@@ -0,0 +1,22 @@
+function autoemail(td) {
+    let txt = td.innerHTML;
+    let array = new Array();
+    let email = "";
+    if (!txt.includes("mailto:")) {
+        for (let i = 0; i < txt.length; i++) {
+            if (txt[i] != '[' && txt[i] != ']' && txt[i] != ',' && txt[i] != ' ') {
+                email += txt[i];
+            } else {
+                if (email != "") array.push(email);
+                email = "";
+            }
+        }
+        email = "";
+        for (let i = 0; i < array.length; i++) {
+            array[i] = "<p><a href=\"mailto:" + array[i] + "\">" + array[i] + "<\/a><\/p>";
+            email += (array[i] + ' ');
+        }
+       
+        td.innerHTML = email;
+    }
+}
